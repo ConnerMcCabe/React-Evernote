9ce38c69e46f50518869db79b2cf0a9b665df1e7
Admin <conner.p.mccabe@gmail.com>
2019-10-30 14:24:38 -0500

    add editor, sidebar, and sidebarItems

---
+++ src/editor/editor.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+import debounce from '../helpers';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+class EditorComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      title: '',
+      id: ''
+    };
+  }
+  render() {
+    return(<h1>Hello from the editor</h1>)
+  }
+}
+export default withStyles(styles)(EditorComponent)
